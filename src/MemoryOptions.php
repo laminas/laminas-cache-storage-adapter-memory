92d9c18f3583e53b6c02263960595d4316ada023
Alex Schloegl <alex@try2catch.com>
2024-03-18 08:08:00 +0100

    Fixed formatting issues

--- src/MemoryOptions.php
+++ src/MemoryOptions.php
@@ -16,102 +16,102 @@ use function strtoupper;
  */
 class MemoryOptions extends AdapterOptions
 {
-	/**
-	 * memory limit
-	 *
-	 * @var null|int
-	 */
-	protected $memoryLimit;
+    /**
+     * memory limit
+     *
+     * @var null|int
+     */
+    protected $memoryLimit;
 
-	/**
-	 * Set memory limit
-	 *
-	 * - A number less or equal 0 will disable the memory limit
-	 * - When a number is used, the value is measured in bytes. Shorthand notation may also be used.
-	 * - If the used memory of PHP exceeds this limit an OutOfSpaceException
-	 *   will be thrown.
-	 *
-	 * @link http://php.net/manual/faq.using.php#faq.using.shorthandbytes
-	 *
-	 * @param  string|int $memoryLimit
-	 * @return MemoryOptions Provides a fluent interface
-	 */
-	public function setMemoryLimit($memoryLimit)
-	{
-		$memoryLimit = $this->normalizeMemoryLimit($memoryLimit);
+    /**
+     * Set memory limit
+     *
+     * - A number less or equal 0 will disable the memory limit
+     * - When a number is used, the value is measured in bytes. Shorthand notation may also be used.
+     * - If the used memory of PHP exceeds this limit an OutOfSpaceException
+     *   will be thrown.
+     *
+     * @link http://php.net/manual/faq.using.php#faq.using.shorthandbytes
+     *
+     * @param string|int $memoryLimit
+     * @return MemoryOptions Provides a fluent interface
+     */
+    public function setMemoryLimit($memoryLimit)
+    {
+        $memoryLimit = $this->normalizeMemoryLimit($memoryLimit);
 
-		if ($this->memoryLimit !== $memoryLimit) {
-			$this->triggerOptionEvent('memory_limit', $memoryLimit);
-			$this->memoryLimit = $memoryLimit;
-		}
+        if ($this->memoryLimit !== $memoryLimit) {
+            $this->triggerOptionEvent('memory_limit', $memoryLimit);
+            $this->memoryLimit = $memoryLimit;
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Get memory limit
-	 *
-	 * If the used memory of PHP exceeds this limit an OutOfSpaceException
-	 * will be thrown.
-	 *
-	 * @return int
-	 */
-	public function getMemoryLimit()
-	{
-		if ($this->memoryLimit === null) {
-			// By default use half of PHP's memory limit if possible
-			$memoryLimit = $this->normalizeMemoryLimit(ini_get('memory_limit'));
-			if ($memoryLimit >= 0) {
-				$this->memoryLimit = (int) ($memoryLimit / 2);
-			} else {
-				// disable memory limit
-				$this->memoryLimit = 0;
-			}
-		}
+    /**
+     * Get memory limit
+     *
+     * If the used memory of PHP exceeds this limit an OutOfSpaceException
+     * will be thrown.
+     *
+     * @return int
+     */
+    public function getMemoryLimit()
+    {
+        if ($this->memoryLimit === null) {
+            // By default use half of PHP's memory limit if possible
+            $memoryLimit = $this->normalizeMemoryLimit(ini_get('memory_limit'));
+            if ($memoryLimit >= 0) {
+                $this->memoryLimit = (int)($memoryLimit / 2);
+            } else {
+                // disable memory limit
+                $this->memoryLimit = 0;
+            }
+        }
 
-		return $this->memoryLimit;
-	}
+        return $this->memoryLimit;
+    }
 
-	/**
-	 * Normalized a given value of memory limit into the number of bytes
-	 *
-	 * @param string|int $value
-	 * @throws Exception\InvalidArgumentException
-	 * @return int
-	 */
-	protected function normalizeMemoryLimit($value)
-	{
-		if ($value === null) {
-			return null;
-		}
+    /**
+     * Normalized a given value of memory limit into the number of bytes
+     *
+     * @param string|int $value
+     * @return int|null
+     * @throws Exception\InvalidArgumentException
+     */
+    protected function normalizeMemoryLimit($value)
+    {
+        if ($value === null) {
+            return null;
+        }
 
-		if (is_numeric($value)) {
-			return (int) $value;
-		}
+        if (is_numeric($value)) {
+            return (int)$value;
+        }
 
-		if (! preg_match('/(\-?\d+)\s*(\w*)/', $value, $matches)) {
-			throw new Exception\InvalidArgumentException("Invalid memory limit '{$value}'");
-		}
+        if (!preg_match('/(\-?\d+)\s*(\w*)/', $value, $matches)) {
+            throw new Exception\InvalidArgumentException("Invalid memory limit '{$value}'");
+        }
 
-		$value = (int) $matches[1];
-		if ($value <= 0) {
-			return 0;
-		}
+        $value = (int)$matches[1];
+        if ($value <= 0) {
+            return 0;
+        }
 
-		switch (strtoupper($matches[2])) {
-			case 'G':
-				$value *= 1024;
-			// no break
+        switch (strtoupper($matches[2])) {
+            case 'G':
+                $value *= 1024;
+            // no break
 
-			case 'M':
-				$value *= 1024;
-			// no break
+            case 'M':
+                $value *= 1024;
+            // no break
 
-			case 'K':
-				$value *= 1024;
-			// no break
-		}
+            case 'K':
+                $value *= 1024;
+            // no break
+        }
 
-		return $value;
-	}
+        return $value;
+    }
 }
--- test/unit/MemoryOptionsTest.php
+++ test/unit/MemoryOptionsTest.php
@@ -8,92 +8,92 @@ use Laminas\Cache\Exception\InvalidArgumentException;
 
 class MemoryOptionsTest extends AbstractCommonAdapterTest
 {
-	public function setUp(): void
-	{
-		// instantiate memory adapter
-		$this->options = new Cache\Storage\Adapter\MemoryOptions();
-		$this->storage = new Cache\Storage\Adapter\Memory();
-		$this->storage->setOptions($this->options);
+    public function setUp(): void
+    {
+        // instantiate memory adapter
+        $this->options = new Cache\Storage\Adapter\MemoryOptions();
+        $this->storage = new Cache\Storage\Adapter\Memory();
+        $this->storage->setOptions($this->options);
 
-		parent::setUp();
-	}
+        parent::setUp();
+    }
 
-	/**
-	 * @param string $iniValue
-	 * @param int $expectedMemoryLimit
-	 * @return void
-	 *
-	 * @runInSeparateProcess
-	 * @dataProvider iniValuesDataSet
-	 */
-	public function testMemoryLimitFromIni($iniValue, $expectedMemoryLimit)
-	{
-		ini_set('memory_limit', $iniValue);
+    /**
+     * @param string $iniValue
+     * @param int $expectedMemoryLimit
+     * @return void
+     *
+     * @runInSeparateProcess
+     * @dataProvider iniValuesDataSet
+     */
+    public function testMemoryLimitFromIni($iniValue, $expectedMemoryLimit)
+    {
+        ini_set('memory_limit', $iniValue);
 
-		$this->assertEquals($expectedMemoryLimit, $this->options->getMemoryLimit());
-	}
+        $this->assertEquals($expectedMemoryLimit, $this->options->getMemoryLimit());
+    }
 
-	/**
-	 * @param string|null $memoryLimit
-	 * @param string $expectedExceptionMessage
-	 * @dataProvider invalidMemoryLimitDataSet
-	 */
-	public function testInvalidMemoryLimitThrowsException($memoryLimit, $expectedExceptionMessage)
-	{
-		$this->expectException(InvalidArgumentException::class);
-		$this->expectExceptionMessage($expectedExceptionMessage);
+    /**
+     * @param string|null $memoryLimit
+     * @param string $expectedExceptionMessage
+     * @dataProvider invalidMemoryLimitDataSet
+     */
+    public function testInvalidMemoryLimitThrowsException($memoryLimit, $expectedExceptionMessage)
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
 
-		$this->options->setMemoryLimit($memoryLimit);
-	}
+        $this->options->setMemoryLimit($memoryLimit);
+    }
 
-	/**
-	 * @param string|int $value
-	 * @param int $expectedMemoryLimit
-	 * @dataProvider validMemoryLimitDataSet
-	 */
-	public function testValidMemoryLimit($memoryLimit, $expectedMemoryLimit)
-	{
-		$this->options->setMemoryLimit($memoryLimit);
+    /**
+     * @param string|int $value
+     * @param int $expectedMemoryLimit
+     * @dataProvider validMemoryLimitDataSet
+     */
+    public function testValidMemoryLimit($memoryLimit, $expectedMemoryLimit)
+    {
+        $this->options->setMemoryLimit($memoryLimit);
 
-		$this->assertEquals($expectedMemoryLimit, $this->options->getMemoryLimit());
-	}
+        $this->assertEquals($expectedMemoryLimit, $this->options->getMemoryLimit());
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function invalidMemoryLimitDataSet()
-	{
-		return [
-			[ 'invalid', "Invalid memory limit 'invalid'" ],
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function invalidMemoryLimitDataSet()
+    {
+        return [
+            ['invalid', "Invalid memory limit 'invalid'"],
+        ];
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function validMemoryLimitDataSet()
-	{
-		return [
-			[ 'M256', 256 ],
-			[ 'G1024', 1024 ],
-			[ 134217728, 134217728 ], // 128M
-			[ 1073741824, 1073741824 ], // 1G
-			[ 1024, 1024 ], // 1k
-			[ '128M', 134217728 ],
-			[ '1G', 1073741824 ],
-			[ '1k', 1024 ],
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function validMemoryLimitDataSet()
+    {
+        return [
+            ['M256', 256],
+            ['G1024', 1024],
+            [134217728, 134217728], // 128M
+            [1073741824, 1073741824], // 1G
+            [1024, 1024], // 1k
+            ['128M', 134217728],
+            ['1G', 1073741824],
+            ['1k', 1024],
+        ];
+    }
 
-	/**
-	 * @return array
-	 */
-	public static function iniValuesDataSet()
-	{
-		return [
-			[ '128M', 67108864 ],
-			[ '1G', 536870912 ],
-			[ '32M', 16777216 ],
-		];
-	}
+    /**
+     * @return array
+     */
+    public static function iniValuesDataSet()
+    {
+        return [
+            ['128M', 67108864],
+            ['1G', 536870912],
+            ['32M', 16777216],
+        ];
+    }
 }
